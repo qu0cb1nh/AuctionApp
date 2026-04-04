d88213ec31d632ef328b4383467240ed335d91bd
qu0cb1nh <25021656@vnu.edu.vn>
2026-04-04 17:33:31 +0700

    chore: refactor

---
+++ src/main/java/net/auctionapp/server/database/DatabaseManager.java
@@ -0,0 +1,52 @@
+package net.auctionapp.server.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import net.auctionapp.common.utils.ConfigUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final DatabaseManager instance = new DatabaseManager();
+    private HikariDataSource dataSource;
+
+    private DatabaseManager() { }
+
+    public static DatabaseManager getInstance() {
+        return instance;
+    }
+
+    public synchronized void createConnectionPool() { // synchronized in case multithreaded server
+        if(dataSource != null) {
+            return;
+        }
+        try {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(ConfigUtil.getDatabaseUrl());
+            config.setUsername(ConfigUtil.getDatabaseUser());
+            config.setPassword(ConfigUtil.getDatabasePassword());
+            config.setMaximumPoolSize(10);
+            config.setMinimumIdle(2);
+            config.setPoolName("AuctionAppHikariPool");
+            dataSource = new HikariDataSource(config);
+        } catch (Exception e) {
+            System.err.println("Failed to initialize database connection pool: " + e.getMessage());
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (dataSource == null || dataSource.isClosed()) {
+            throw new SQLException("Data source is not initialized or has been closed.");
+        }
+        return dataSource.getConnection();
+    }
+
+    public synchronized void closeConnectionPool() { // synchronized in case multithreaded server
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+}
+
