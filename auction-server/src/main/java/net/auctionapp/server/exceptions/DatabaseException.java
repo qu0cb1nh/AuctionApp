7be15232edbc1457e1358af9df42de8f38d7dc5e
NguyenDucAnh <25021625@vnu.edu.vn>
2026-05-12 22:19:33 +0700

    feat: deposit + withdrawal

--- auction-server/src/main/java/net/auctionapp/server/exceptions/DatabaseException.java
+++ auction-server/src/main/java/net/auctionapp/server/exceptions/DatabaseException.java
@@ -1,6 +1,10 @@
 package net.auctionapp.server.exceptions;
 
 public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+
     public DatabaseException(String message, Throwable cause) {
         super(message, cause);
     }
