e241c8e80be2e0ef8e9271d17dc976b8fcfbd3e5
DangTuanDuc_25021726_TDTT <25021726@vnu.edu.vn>
2026-05-24 17:15:28 +0700

    feat: notify watchers before auctions end

--- auction-common/src/main/java/net/auctionapp/common/notifications/NotificationType.java
+++ auction-common/src/main/java/net/auctionapp/common/notifications/NotificationType.java
@@ -3,5 +3,6 @@ package net.auctionapp.common.notifications;
 public enum NotificationType {
     OUTBID,
     AUCTION_WON,
-    AUCTION_SELLER_RESULT
+    AUCTION_SELLER_RESULT,
+    WATCH_LIST_ENDING_SOON
 }
--- auction-server/src/main/java/net/auctionapp/server/dao/WatchListDao.java
+++ auction-server/src/main/java/net/auctionapp/server/dao/WatchListDao.java
@@ -5,5 +5,7 @@ import java.util.List;
 public interface WatchListDao {
     List<String> findAuctionIdsByUserId(String userId);
 
+    List<String> claimEndingSoonReminderRecipients(String auctionId);
+
     void setWatched(String userId, String auctionId, boolean watched);
 }
